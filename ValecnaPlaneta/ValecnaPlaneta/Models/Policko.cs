436f1128cbe51fc52d663fa0cdab4ab3a8835f71
Pavel Kotek <kotekp21@spstrutnov.cz>
2023-05-22 11:19:43 +0200

    Musím to nechat na kroužek, nevím nic

--- ValecnaPlaneta/ValecnaPlaneta/Models/Policko.cs
+++ ValecnaPlaneta/ValecnaPlaneta/Models/Policko.cs
@@ -8,7 +8,6 @@ namespace ValecnaPlaneta.Models
         public int Id { get; set; }
         [Required]
         virtual public Hra HraKamPatri { get; set; }
-        public int cislo { get; set; }
         public enum Stav { Prazdno, Zabrano, Bunkr } 
         public string Vlastnik { get; set; }
     }
