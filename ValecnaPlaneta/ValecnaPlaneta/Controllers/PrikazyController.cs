2b275e4b8a8f82fe992aef4ef29581a8af2ec90d
Kačer Šimon <kacers21@spstrutnov.cz>
2023-06-05 10:28:05 +0200

    Enginu se posílá zvlášť příkaz a políčko

--- ValecnaPlaneta/ValecnaPlaneta/Controllers/PrikazyController.cs
+++ ValecnaPlaneta/ValecnaPlaneta/Controllers/PrikazyController.cs
@@ -18,33 +18,40 @@ namespace ValecnaPlaneta.Controllers
 
             zadanyPrikaz = zadanyPrikaz.Trim().ToLower();
 
-            //string[] slovaVPrikazu = zadanyPrikaz.Split(' ');   
-            //zadanyPrikaz = (slovaVPrikazu[0] + " " + slovaVPrikazu[1]);
-
-
             if (zadanyPrikaz == "income")
                 return Redirect("/Engine/Prijem");
 
             else if (zadanyPrikaz == "capital")
                 return Redirect("/Engine/Kapital");
 
-            else if (zadanyPrikaz == "send scout")
-                return Redirect("/Engine/PoslatScouta");
-
-            else if (zadanyPrikaz == "send soldier")
-                return Redirect("/Engine/PoslatVojaka");
-
-            else if (zadanyPrikaz == "send infiltrator")
-                return Redirect("/Engine/PoslatInfiltratora");
-
-            else if (zadanyPrikaz == "send miner")
-                return Redirect("/Engine/PoslatTezebniJednotku");
-
             else if (zadanyPrikaz == "help")
             {
-                ViewData["help"] = "Available commands: Help, Income, Capital, Send + soldier/scout/miner/infiltrator + písmeno políčka a číslo políčka";
+                ViewData["help"] = "Available commands: Help, Income, Capital, Send + soldier/scout/miner/infiltrator + number of position";
                 return View();
             }
+
+            string[] slovaVPrikazu = zadanyPrikaz.Split(' ');
+            zadanyPrikaz = (slovaVPrikazu[0] + " " + slovaVPrikazu[1]);
+
+            int cislo;
+            if (slovaVPrikazu.Length < 3 || !int.TryParse(slovaVPrikazu[2],out cislo))
+            { 
+                ViewData["chyba"] = "Wrong syntax. Write Help for more information.";
+                return View();
+            }
+
+            if (zadanyPrikaz == "send scout")
+                return Redirect("/Engine/PoslatScouta/" + slovaVPrikazu[2]);
+
+            else if (zadanyPrikaz == "send soldier")
+                return Redirect("/Engine/PoslatVojaka/" + slovaVPrikazu[2]);
+
+            else if (zadanyPrikaz == "send infiltrator")
+                return Redirect("/Engine/PoslatInfiltratora/" + slovaVPrikazu[2]);
+
+            else if (zadanyPrikaz == "send miner")
+                return Redirect("/Engine/PoslatTezebniJednotku/" + slovaVPrikazu[2]);
+
             else
             {
                 ViewData["chyba"] = "Wrong syntax. Write Help for more information.";
